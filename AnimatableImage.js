425272401fbe4e9c75a834403ff142219e29284b
techoutliers <67784003+techoutliers@users.noreply.github.com>
2021-06-25 14:18:16 +0900

    Update AnimatableImage.js

--- AnimatableImage.js
+++ AnimatableImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Animated, ImageBackground } from 'react-native';
-import Image from "react-native-image-progress";
+import Image from "react-native-fast-image";
 
 function AnimatableImage(props) {
   const { animated, children, ...rest } = props;
